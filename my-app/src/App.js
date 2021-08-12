0a0e94231865431d5743f26ab18ae6a4a925a7de
chbi2019 <chbi@sheridancollege.ca>
2021-08-12 17:49:45 -0400

    Update App.js 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Richard Chen 123 Edit <code>src/App.js</code> and save to reload.
+          Richard Chen Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
