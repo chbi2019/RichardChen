04a9aa918e8303d05a3724177544d2698719e33b
chbi2019 <chbi@sheridancollege.ca>
2021-08-27 13:00:45 -0400

    Completed four pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/contact';
 
 function App() {
   return (
@@ -9,9 +13,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact=''>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
+          <Route path='/portfolio' exact component={Portfolio} />
+          <Route path='/contact' exact component={Contact}/>
             
-          </Route>
+          
         </Switch>
       </Router>
 
